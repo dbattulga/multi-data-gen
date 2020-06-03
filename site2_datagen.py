48af19173832823a4f63a608d9a299e251d535df
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2020-06-03 21:27:50 +0200

    added generators

---
+++ site2_datagen.py
@@ -0,0 +1,83 @@
+from __future__ import absolute_import
+
+from mqtt import MqttPublishHandler
+import sys
+import time
+import csv
+import os
+import schedule
+
+
+line = []
+
+line.append("car_make")
+line.append("car_model")
+line.append("car_model")
+line.append("engine_on")
+line.append("otonomo_id")
+line.append("geocoding_city")
+line.append("time")
+line.append("latitude")
+line.append("longitude")
+line.append("heading")
+line.append("speed")
+line.append("provider")
+line.append("geocoding_country_long")
+line.append("geocoding_state")
+line.append("geocoding_town")
+line.append("ignition")
+line.append("geocoding_suburb")
+line.append("geocoding_road")
+
+
+# mqtt_address = str(sys.argv[1])
+# runtime = str(sys.argv[2])
+# gen_rate = str(sys.argv[3])
+
+run_at = str(sys.argv[1])
+mqtt_address = "127.0.0.1"
+t4 = "T-4"
+t5 = "T-5"
+t6 = "T-6"
+gen_rate = "10"
+runtime = "600"
+
+def job():
+    mqttph1 = MqttPublishHandler(mqtt_address, t4, 'mqtt-pub', 'mqtt-pub') #host, client id, username & password
+    mqttph1.connect()
+    mqttph2 = MqttPublishHandler(mqtt_address, t5, 'mqtt-pub', 'mqtt-pub') #host, client id, username & password
+    mqttph2.connect()
+    mqttph3 = MqttPublishHandler(mqtt_address, t6, 'mqtt-pub', 'mqtt-pub') #host, client id, username & password
+    mqttph3.connect()
+
+    gen_irate = int(gen_rate)
+    iruntime = int(runtime)
+
+    end = time.time() + iruntime
+    while time.time() < end:
+        message = "sda" + ":" + line[12] + ":" + str(time.time()) + ":" + line[6] + ":" + line[7] + ":" + line[0] + ":" + \
+                  line[1] + ":" + line[2] \
+                  + ":" + line[3] + ":" + line[4] + ":" + line[5] + ":" + line[2] + ":" + line[8] + ":" + line[8] + ":" + \
+                  line[10] + ":" + line[11] \
+                  + ":" + line[13] + ":" + line[14] + ":" + line[15] + ":" + line[16]
+        #message1 = "time:"+ str(time.time()) + ":"+ t1
+        #message2 = "time:" + str(time.time()) + ":" + t2
+        #message3 = "time:" + str(time.time()) + ":" + t3
+        time.sleep(gen_irate/1000) #ms
+        #print(message1)
+        #print(message2)
+        #print(message3)
+        mqttph1.publish(t4, message)
+        mqttph2.publish(t5, message)
+        mqttph3.publish(t6, message)
+
+    mqttph1.disconnect()
+    mqttph2.disconnect()
+    mqttph3.disconnect()
+    return schedule.CancelJob
+
+schedule.every().day.at(run_at).do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
